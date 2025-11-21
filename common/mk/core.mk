49a54c4e65a72a2f3e4e71fef5231b7fb165e1ef
Adam Buxton <buxton.adam@gmail.com>
2025-11-21 15:48:13 +0000

    add example import and layered make

---
+++ common/mk/core.mk
@@ -0,0 +1,18 @@
+# https://github.com/krisnova/Makefile/blob/main/Makefile
+
+-include .env
+REPO_TOP=$(shell git rev-parse --show-toplevel)
+CORE=${REPO_TOP}/common/mk/core.mk
+BIN_DIR=${REPO_TOP}/common/bin
+
+
+all: help help-git
+
+
+-include ${REPO_TOP}/common/bin/git.mk
+
+.PHONY: help
+help:  ## Show help messages for make targets
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(firstword $(CORE)) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[32m%-30s\033[0m %s\n", $$1, $$2}'
+
+
